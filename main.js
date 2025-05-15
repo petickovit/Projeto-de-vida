a0b36b3c526822d5d0c915f9a69825b98ca169a9
petickovit <160601349+petickovit@users.noreply.github.com>
2025-05-15 11:50:02 -0300

    Add files via upload

--- main.js
+++ main.js
@@ -1,16 +1,34 @@
 const botoes= document.querySelectorAll(".botao");
 const textos= document.querySelectorAll(".aba-conteudo");
 
-for (let i=0; i <botoes.length; i++) 
+for (let i=0; i <botoes.length; i++){
     botoes [i].onclick=function(){
 
         for (let j=0; j <botoes.length; j++){
             botoes[j].classList.remove("ativo");
             textos[j].classList.remove("ativo");
-        
+       }
         botoes[i].classList.add("ativo");
         textos[i].classList.add("ativo");
     }
     }
 const contadores=document.querySelectorAll(".contador");
-contadores[0].textContent="contagem regressiva";
+const tempoObjetivo1=new Date("2022-10-05T00:00:00");
+const tempoObjetivo2=new Date("2025-12-05T00:00:00")
+const tempoObjetivo3=new Date("2025-12-30T00:00:00")
+const tempoObjetivo4=new Date("2026-02-01T00:00:00")
+
+const tempos=[tempoObjetivo1,tempoObjetivo2,tempoObjetivo3,tempoObjetivo4]
+function calculatempo(tempoObjetivo){
+    let tempoAtual = new date ();
+    let tempoFinal = tempoObjetivo - tempoAtual;
+    let segundos = Math.floor(tempoFinal/1000);
+    let minutos = Math.floor(segundos/60);
+    let horas = Math.floor(minuto/60);
+    let dia = math.floor(horas/24)
+
+    segundos %=60;
+    minutos %=60;
+    horas %=24
+    if ()
+}
